70d6b97ef4842071b79b36549b4b7e89e0e43fb9
wan-liyang <51030647+wan-liyang@users.noreply.github.com>
2023-06-04 15:43:30 +0800

    sample for upload & download via API

---
+++ UploadTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace UploadTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        byte[] bytes = File.ReadAllBytes("test file.txt");
+        using (var client = new HttpClient())
+        {
+            using (var content = new MultipartFormDataContent())
+            {
+                content.Add(new StreamContent(new MemoryStream(bytes)), "files", "test file.txt");
+
+                using (var message = client.PostAsync("url", content))
+                {
+                    var result = message.Result.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+            }
+        }
+
+
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
+    {
+        using (var client = new HttpClient())
+        {
+            using (var message = client.GetAsync("url"))
+            {
+                var result = message.Result.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            }
+        }
+
+
+        Assert.Pass();
+    }
+}
---
+++ WebApi-UploadFile/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_UploadFile.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class UploadController : ControllerBase
+	{
+		[HttpGet]
+		public FileResult Download()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(@"/Users/liyang/Documents/LearnSomething/LearnSomething-asp.net/WebApi-UploadFile/test file.txt");
+            string fileName = "test file.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+		[HttpPost]
+        public async Task<IActionResult> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
+
